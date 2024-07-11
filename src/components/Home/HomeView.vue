3327e20bd8bf0709e0b45374c46b4682d811b136
Shade <anhvu_2027@depauw.edu>
2024-07-11 15:27:24 +0700

    Added text to Home Screen

--- src/components/Home/HomeView.vue
+++ src/components/Home/HomeView.vue
@@ -7,7 +7,15 @@
       <section id="banner">
         <div class="inner">
           <h2>Organic Chemistry E-Text</h2>
-          <p>adfsafsadfsafdsaffdadfa</p>
+          <p style="text-align: justify; display: flex; align-self: center; text-align-last: center;">
+            Welcome to the Chem 120 Organic Chemistry Electronic Textbook. 
+            You may have encountered electronic textbooks before. 
+            In most cases you probably found that they were basically 
+            paper textbooks that had been digitized and presented electronically. 
+            Unlike those electronic textbooks, this one attempts to take full advantage 
+            of the digital medium by incorporating not only images, but video, games, 
+            3D interactive objects and more.
+          </p>
         </div>
       </section>
 
@@ -18,8 +26,14 @@
             <div class="inner">
               <a href="#" class="image"><img src="@/assets/cat2.jpg" alt="" /></a>
               <div class="content">
-                <h2 class="major main-content" id="main-content1">Magna arcu feugiat</h2>
-                <p class="main-content">Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
+                <h2 class="major main-content" id="main-content1">Purpose of the project</h2>
+                <p class="main-content" style="text-align: justify; text-align-last: end;">
+                  Why do this? Well, you will see as we progress through the semester that organic
+                  chemistry is a very visual subject. After all, organic compounds, which are the focus of 
+                  our study this semester, are three dimensional objects. 
+                  Understanding their composition, their properties, and their chemistry will 
+                  require us to understand their 3D shapes. 
+                </p>
               </div>
             </div>
           </section>
@@ -29,8 +43,14 @@
             <div class="inner">
               <a href="#" class="image"><img src="@/assets/cat2.jpg" alt="" /></a>
               <div class="content" id="content2">
-                <h2 class="major main-content" id="main-content2">Tempus adipiscing</h2>
-                <p class="main-content" id="main-content3">Lorem ipsum dolor sit amet, etiam lorem adipiscing elit. Cras turpis ante, nullam sit amet turpis non, sollicitudin posuere urna. Mauris id tellus arcu. Nunc vehicula id nulla dignissim dapibus. Nullam ultrices, neque et faucibus viverra, ex nulla cursus.</p>
+                <!--<h2 class="major main-content" id="main-content2">The importance of 2D model</h2>--->
+                <p class="main-content" id="main-content3">
+                  However, we often need to represent organic 
+                  compounds on 2D surfaces - a piece of paper, a computer screen, or a whiteboard. 
+                  One of our challenges will be to learn how to represent these molecules in 2D while having a 
+                  3D understanding of them. This e-textbook attempts to bridge the gap between 2D pictures and 
+                  3D understanding.
+                </p>
               </div>
             </div>
           </section>
@@ -40,40 +60,38 @@
       <section id="footer">
         <div class="inner">
           <h2 class="major">Get in touch</h2>
-          <p>Cras mattis ante fermentum, malesuada neque vitae, eleifend erat. Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.</p>
+          <p>If you have any questions, please feel free to contact: </p>
           <form method="post" action="#">
             <div class="fields">
-              <div class="field">
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" />
-              </div>
               <div class="field">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" />
               </div>
               <div class="field">
+                <label for="topic">Topic</label>
+                <input type="topic" name="topic" id="topic" />
+              </div>
+              <div class="field">
                 <label for="message">Message</label>
                 <textarea name="message" id="message" rows="4"></textarea>
               </div>
+              
             </div>
             <ul class="actions">
               <li><input type="submit" value="Send Message" /></li>
             </ul>
           </form>
           <ul class="contact">
-            <li class="icon solid fa-home">
-              Untitled Inc<br />
-              1234 Somewhere Road Suite #2894<br />
-              Nashville, TN 00000-0000
+            <li class="icon solid fa-home" style="display: flex !important; align-items: start !important; justify-content: flex-start !important;">
+              Julian Science and Math Center, Room 360, <br>
+              Greencastle, IN 46135  
             </li>
-            <li class="icon solid fa-phone">(000) 000-0000</li>
-            <li class="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>
-            <li class="icon brands fa-twitter"><a href="#">twitter.com/untitled-tld</a></li>
-            <li class="icon brands fa-facebook-f"><a href="#">facebook.com/untitled-tld</a></li>
-            <li class="icon brands fa-instagram"><a href="#">instagram.com/untitled-tld</a></li>
+            <li class="icon brands fa-twitter">Office Hours: </li>
+            <li class="icon solid fa-phone">(765) 658-4613</li>
+            <li class="icon solid fa-envelope"><a href="#">jhansen@depauw.edu</a></li>
           </ul>
           <ul class="copyright">
-            <li>&copy; Untitled Inc. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            <li>Copyright &copy;  2024 Dr. Jeffrey A. Hansen -  All Rights Reserved.</li><li>Template: <a href="http://html5up.net">HTML5 UP</a></li>
           </ul>
         </div>
       </section>
